2f77e810e43dafeba4be102b67a2bbd9d7ab75cd
John Alexis Guerra Gómez <john.guerra@gmail.com>
2019-02-01 15:32:26 -0500

    Adding Readme

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Here is a simple example on how to use node+express
 npm install
 npm start
 ```
+
+And then point your browser to http://localhost:3000
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require("express");
 var app = express();
 
 
-
+app.use(express.static("public"));
 
 app.get("/api", function(req, res) {
     console.log("Request on /");
