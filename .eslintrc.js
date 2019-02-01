77abf08f3de21a5ff62ecc8fea44cc34cbf2c056
John Alexis Guerra Gómez <john.guerra@gmail.com>
2019-02-01 15:18:41 -0500

    Adding eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 5
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": "off"
+    }
+};
--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 var express = require("express");
 
 
-
 var app = express();
 
-
 app.get("/", function(req, res) {
-	console.log("Request on /");
-	res.send("Hola World!");
+    console.log("Request on /");
+    res.send("Hola World!");
 });
 
 
+
 app.listen(3000, function () {
-	console.log("Server listing for connections on port 3000");
+    console.log("Server listing for connections on port 3000");
 });
